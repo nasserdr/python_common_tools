1390de675dfc0268a27b471e1f0e96e4b6c841f9
Dr Hassan Nasser <hassan.nasser@me.com>
2021-01-20 21:45:54 +0000

    file 6 added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i == 98:
+for i in range(0, 100):
+    if i == 99:
         print(i)
     else:
-        print("{:02d}".format(i), end=' ,')
+        print("{:02d}".format(i), end=', ')
