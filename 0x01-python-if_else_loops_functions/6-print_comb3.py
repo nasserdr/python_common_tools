13587fb881eeb2277ec5c3fe604048e7e2eec799
Dr Hassan Nasser <hassan.nasser@me.com>
2021-01-20 21:55:29 +0000

    file 6 added

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(i, 10):
-        if(i == 9):
+    for j in range(i + 1, 10):
+        if(i == 8):
             print("{:d}{:d}".format(i, j))
         else:
             print("{:d}{:d}".format(i, j), end=', ')
