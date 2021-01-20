1f43c7c454313180fa4b2ce5bc0182d3dc95f863
Dr Hassan Nasser <hassan.nasser@me.com>
2021-01-20 22:56:26 +0000

    file 11 added

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,17 @@
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    elif b < 0:
+        pow = 1
+        while (b < 0):
+            pow *= a
+            b += 1
+        return 1/pow
+    else:
+        pow = 1
+        while (b > 1):
+            pow *= a
+            b -= 1
+        return pow
