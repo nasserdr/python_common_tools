35d9f45e032c18db68aba2d98f98018acb9a2ffe
Dr Hassan Nasser <hassan.nasser@me.com>
2021-01-20 21:12:27 +0000

    1 updated

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,4 @@ elif number == 0:
     print('Last digit of {:d} is {:d} and is 0'.format(number, last))
 else:
     print('Last digit of {:d} is {:d} and is less than 6 and\
-    not 0'.format(number, last))
+not 0'.format(number, last))
