075f0dab6d429a079c5e6b091ad28262787630c1
drnasser <hassan.nasser@me.com>
2021-02-05 22:31:20 +0000

    file 2 added

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    elif idx >= len(my_list):
+    elif idx > len(my_list):
         return None
     else:
         my_list[idx] = element
