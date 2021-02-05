02d2867b05f5383f0fee818ba1fafcddbc4f3b9e
drnasser <hassan.nasser@me.com>
2021-02-05 20:44:43 +0000

    file 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print('0 arguments')
+        print('0 arguments.')
     elif len(sys.argv) == 2:
         print('1 argument:')
         print('1: {}'.format(sys.argv[1]))
