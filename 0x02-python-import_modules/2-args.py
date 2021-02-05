5e36d668d755e93fc580e6acf28c7eb43d4a9f8b
drnasser <hassan.nasser@me.com>
2021-02-05 20:45:47 +0000

    file 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print('{} arguments:'.format(len(sys.argv)-1))
         for i in range(len(sys.argv)-1):
-            print('{} : {}'.format(i+1, sys.argv[i+1]))
+            print('{}: {}'.format(i+1, sys.argv[i+1]))
