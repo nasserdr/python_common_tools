88f759f54fa07bb3ae5d91c998d31566ce121aaf
drnasser <hassan.nasser@me.com>
2021-02-06 22:27:27 +0000

    odif

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+def delete_at(my_list=[], idx=0):
+
+    if idx == 0 or idx >= len(my_list) - 1:
+        return my_list
+    else:
+        new_list = []
+        i = 0
+        for i in range(0, len(my_list)):
+            if i == idx:
+                i = i + 1
+            else:
+                new_list.append(my_list[i])
