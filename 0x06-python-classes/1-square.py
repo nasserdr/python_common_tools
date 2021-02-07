5ff3c4ba44700d412089d5b0994b13ef6762f7bc
nasserdr <hassan.nasser@me.com>
2021-02-07 19:20:53 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,4 +9,4 @@ class Square:
     """an empty class Square that defines a square """
     def __init__(self, size):
         """ To initialize the size of the squere"""
-        self.size = size
+        self.__size = size
