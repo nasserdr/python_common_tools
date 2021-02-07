5bce477b8ac2ebd516abbb820846b2e707517f1a
nasserdr <hassan.nasser@me.com>
2021-02-07 16:15:51 +0000

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    nb = 0
+    for i in range(0, x):
+        try:
+            print('{}'.format(my_list[i]), end='')
+            nb += 1
+        except IndexError:
+            continue
+    print("")
+    return nb
