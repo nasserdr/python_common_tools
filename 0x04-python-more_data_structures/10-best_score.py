a18d80d3e64f27b15278e7e3c7316decd3d1b855
nasserdr <hassan.nasser@me.com>
2021-02-07 15:17:14 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if bool(a_dictionary) == False:
+    if bool(a_dictionary) is False:
         return None
     else:
         max_key = list(a_dictionary.keys())[0]
