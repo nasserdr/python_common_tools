6b29487922776d31a66a41f78cfc486d5f0db9dc
nasserdr <hassan.nasser@me.com>
2021-02-07 21:16:00 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+A class defined by it's size with a default value
+"""
+
+
+class Square:
+    """
+    an empty class Square that defines a square 
+    """
+    def __init__(self, size=0):
+        """
+        To initialize the size of the square
+        """
+        try:
+            if type(size) is not int:
+                raise TypeError
+            if size < 0:
+                raise ValueError
+            self.__size = size
+        except ValueError:
+            print('size must be >0')
+        except TypeError:
+            print('size must be an integer')
+
+    def area(self):
+        return self.__size**2
