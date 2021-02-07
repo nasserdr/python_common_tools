2a6f42ddc6c9258e47088a69f2811bda75f058a8
nasserdr <hassan.nasser@me.com>
2021-02-07 19:16:12 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+A class defined by it's size
+"""
+
+
+class Square:
+    """an empty class Square that defines a square """
+    def __init__(self, size):
+        self.size = size
+
