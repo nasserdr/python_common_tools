3db89fd80106a9cff917c25c0e66c8e684c746c5
nasserdr <hassan.nasser@me.com>
2021-02-07 15:15:59 +0000

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if bool(a_dictionary) == False:
+        return None
+    else:
+        max_key = list(a_dictionary.keys())[0]
+        for k, v in a_dictionary.items():
+            if a_dictionary[k] > a_dictionary[max_key]:
+                max_key = k
+        return max_key
