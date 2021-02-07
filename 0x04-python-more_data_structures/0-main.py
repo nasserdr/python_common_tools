1af6446531c814bcf59325e5c2c396177c90e60d
nasserdr <hassan.nasser@me.com>
2021-02-07 13:58:06 +0000

    modif

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -2,9 +2,9 @@
 square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [2, 1, 2, 3],
+    [3, 4, 5, 6],
+    [5, 7, 8, 9]
 ]
 
 new_matrix = square_matrix_simple(matrix)
