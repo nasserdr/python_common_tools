e5b83396ef16a001cf949829202e5ab33f634532
nasserdr <hassan.nasser@me.com>
2021-02-07 17:54:33 +0000

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as te:
-        print(te)
+        sys.stderr = te
         return False
