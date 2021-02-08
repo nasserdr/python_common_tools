c6bc08b864d056487e249234e2ed980bd4b2adb6
nasserdr <hassan.nasser@me.com>
2021-02-08 20:18:39 +0000

    5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+str = "Holberton School"
+print("{}{}{}".format(str, str, str))
+print("{}".format(str[0:8]))
