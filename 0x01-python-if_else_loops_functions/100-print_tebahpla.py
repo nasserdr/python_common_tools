d6b9147cf15354694826e283141695a9e4826016
nasserdr <hassan.nasser@me.com>
2021-02-08 21:50:32 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,10 +2,6 @@
 c = 122
 upper = False
 while(c >= 97):
-    if(upper is False):
-        print('{}'.format(chr(c)), end='')
-    else:
-        print('{}'.format(chr(c-32)), end='')
+    print('{}'.format(chr(c-32*upper)), end='')
     c -= 1
     upper = not upper
-print('\n')
