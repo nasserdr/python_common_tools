8d80ec1862db560388e0e20400111490956ab8d2
nasserdr <hassan.nasser@me.com>
2021-02-09 17:53:22 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum = my_list[0]
-    if len(my_list) == 1:
-        return sum
+    if len(my_list) == 0:
+        return 0
+    elif len(my_list) == 1:
+        return my_list[0]
     else:
+        sum = my_list[0]
         for i in range(1, len(my_list)):
             if my_list[i] not in my_list[0:i]:
                 sum += my_list[i]
