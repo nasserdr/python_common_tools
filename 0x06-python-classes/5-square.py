8fa83981597409d599ba72d03fff666f64dbc4a0
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-11 22:13:21 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,12 +40,8 @@ class Square:
         """
         return self.__size**2
 
-
     def my_print(self):
-        """
-        print the # in squares
-        """
-        if self.__size==0:
+        if self.__size == 0:
             print("\n")
         else:
             for i in range(0, self.__size):
