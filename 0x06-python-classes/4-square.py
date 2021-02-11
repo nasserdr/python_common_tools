8ab10ff1e28d75184033111d245c30c0e75ee154
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-11 22:01:08 +0100

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+ class defined by it's size with a default value
+"""
+
+
+class Square:
+    """
+    defines the square class
+    """
+
+    def __init__(self, size):
+        """
+        the initialized with the argument size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        The getter property for the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        The setter of the size
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value <= 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        returns the area
+        """
+        return self.__size**2
