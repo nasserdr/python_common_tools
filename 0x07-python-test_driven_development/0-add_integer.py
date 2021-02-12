2b34b65fc6857c016870c576246714086edb6824
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-12 23:51:43 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+To test it:
+>>> add_integer(5,10)
+15
+"""
+
+
 def add_integer(a, b=98):
     """
     Adds 98 to the integer a
@@ -15,3 +22,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return a+b
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    doctest.testfile('tests/0-add_integer.txt')
