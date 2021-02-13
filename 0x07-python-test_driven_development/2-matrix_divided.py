bc47922bb1afde0b2da4a16bdd219de3002fdff4
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-13 11:04:11 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+Module for matrix division
+"""
+
+
 def matrix_divided(matrix, div):
+
+    """
+    Function for matri division
+    """
 
     len_wos = len(matrix[0])
     for i in range(0, len(matrix)):
         if len(matrix[i]) != len_wos:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of\
+            the matrix must have the same size")
 
     if type(div) not in [float, int]:
         raise TypeError("div must be a number")
@@ -13,17 +23,18 @@ def matrix_divided(matrix, div):
         raise ValueError("division by zero")
 
     if type(matrix) is not list and type(matrix[0]) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix\
+        (list of lists) of integers/floats")
 
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             if type(matrix[i][j]) is not int:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of \
+                lists) of integers/floats")
 
     Matrix = [[0 for x in range(len(matrix[0]))] for y in range(len(matrix))]
 
     for i in range(0, len(Matrix)):
         for j in range(0, len(Matrix[0])):
             Matrix[i][j] = round(matrix[i][j]/div, 2)
-
     return Matrix
