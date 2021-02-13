a28d65536b52cff4b07fb4ee2fb6e7965cde6602
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-13 23:45:16 +0100

    Update 0-lockboxes.py

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    index = 0
+    total = list(set(boxes[0])| {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes,total[index:]):
+            if j not in total:
+                total.append(j)
+                index +=1
+                added= True
+    return len(total)==len(boxes)
+
+
+def join(T,R):
+    res =[]
+    for e in R:
+        res += T[e]
+    return res
