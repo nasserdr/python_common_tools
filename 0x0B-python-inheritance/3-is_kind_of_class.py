d8555825633486093bc5432a7896c6787df21ff2
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 09:08:28 +0100

    Update 3-is_kind_of_class.py

--- 0x0B-python-inheritance/3-is_kind_of_class.py
+++ 0x0B-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+returns True if the object is an instance of a class that inherited (directly or indirectly) 
+from the specified class ; otherwise False
+"""
+
+
 def is_kind_of_class(obj, a_class):
     """
     Check if the object of the same class
