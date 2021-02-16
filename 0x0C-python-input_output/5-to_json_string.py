f807434fe7224dd00fb57eab1a46e42388729390
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 20:12:49 +0100

    Create 5-to_json_string.py

---
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Create JSON representations
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns a JSON representation of my_obj
+    """
+    return json.dumps(my_obj)
