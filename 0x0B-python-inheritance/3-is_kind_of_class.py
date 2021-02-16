6dff11bf94f5461bad78b1015aec6a246c4a15e1
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 09:09:09 +0100

    Update 3-is_kind_of_class.py

--- 0x0B-python-inheritance/3-is_kind_of_class.py
+++ 0x0B-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-returns True if the object is an instance of a class that inherited (directly or indirectly) 
-from the specified class ; otherwise False
+returns True if the object is an instance of a class that inherited 
+(directly or indirectly) from the specified class ; otherwise False
 """
 
 
