95573bd63bf961bbf6cf62732078e8f19128f575
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 20:03:54 +0100

    Update 3-write_file.py

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -8,6 +8,6 @@ def write_file(filename="", text=""):
     """
     writes text in filename
     """
-    with open(filename, mode='w', encoding='UTF-8') as MyFile:
+    with open(filename, mode='w', encoding='utf-8') as MyFile:
         MyFile.write(text)
         return(len(text))
