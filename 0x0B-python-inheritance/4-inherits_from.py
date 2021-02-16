9535754c5954bab760e01373d1fc060e3c64b60b
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 09:16:09 +0100

    Update 4-inherits_from.py

--- 0x0B-python-inheritance/4-inherits_from.py
+++ 0x0B-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """
     Return true if obj inerits from a_class
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
