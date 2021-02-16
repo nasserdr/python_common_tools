420294a31b1163117e52b1d4fb618747da2306f7
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 09:30:07 +0100

    Update 0-read_file.py

--- 0x0C-python-input_output/0-read_file.py
+++ 0x0C-python-input_output/0-read_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Reading lines
+"""
+
+
+def read_file(filename=""):
+    """
+    Read all the lines in filename
+    """
+    with open('my_file_0.txt', 'r', encoding = "utf-8") as myFile:
+        for lines in myFile:
+            print(lines, end='')
