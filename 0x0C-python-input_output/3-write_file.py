793a7174d7fc759bb1aca8ef61f5aa74ab2861d6
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-16 19:59:48 +0100

    Create 3-write_file.py

---
+++ 0x0C-python-input_output/3-write_file.py
@@ -0,0 +1,12 @@
+"""
+Writing in a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes text in filename
+    """
+    with open(filename, mode='w', encoding='UTF-8') as MyFile:
+        MyFile.write(text)
+        return(len(text))
