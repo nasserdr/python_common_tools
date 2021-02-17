3e366a95538f59406c1e9b63479ffe4a1bb64297
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-17 19:53:58 +0100

    Update 1-my_list.py

--- 0x0B-python-inheritance/1-my_list.py
+++ 0x0B-python-inheritance/1-my_list.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
-Return a sorted list
+Write a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int
+You are not allowed to import any module
 """
 
 
