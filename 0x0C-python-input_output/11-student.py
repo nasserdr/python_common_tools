fa6727f9ef4f4b7d3262d44ccc4de6eeaaafd9e9
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-18 20:50:44 +0100

    Create 11-student.py

---
+++ 0x0C-python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+The student class
+"""
+import json
+
+
+class Student:
+    """
+    Here we define and develop the student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return json.loads(json.dumps(self.__dict__))
