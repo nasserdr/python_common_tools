04bffebd804ac37d1d90a46294887716d65dce8f
Dr Hassan Nasser <hassan.nasser@me.com>
2021-02-18 20:53:11 +0100

    Update 11-student.py

--- 0x0C-python-input_output/11-student.py
+++ 0x0C-python-input_output/11-student.py
@@ -11,9 +11,15 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
+        """
+        Initialiser
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        Transforms to json
+        """"
         return json.loads(json.dumps(self.__dict__))
