ba137011d35cc4cd8f811b3180e6b91814bc43ce
nasserdr <hassan.nasser@me.com>
2021-02-24 19:59:34 +0100

    updated min op

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -9,7 +9,7 @@ def minOperations(n):
     Finds the minimum number of minOperations
     """
     res = 0
-    for i in range(2, n):
+    for i in range(2, n+1):
         while n % i == 0:
             res += i
             n = n/i
