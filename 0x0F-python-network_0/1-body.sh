fc1f78901fdb17da956bc9099725001e47ba05a3
nasserdr <hassan.nasser@me.com>
2021-02-28 15:30:00 +0100

    updated 1

--- 0x0F-python-network_0/1-body.sh
+++ 0x0F-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the body only if it has a "200" response
-curl -sI $1 | grep 200
+if [ $(curl -s -X GET 0.0.0.0:5000/route_1 -o /dev/null -w %{http_code}) -eq 200 ]; then curl -sb 0.0.0.0:5000/route_1; fi; 
