cb54255182d2337ed3a79aae974b04490ab19f9b
nasserdr <hassan.nasser@me.com>
2021-03-01 18:01:22 +0100

    updated 0

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- Content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    print("    - type: {}".format(type(html)))
+    print("    - Content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf-8')))
