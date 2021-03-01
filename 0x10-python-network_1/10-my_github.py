7be995097955b0aaa2e39d36f3b491e1de4a0a2b
nasserdr <hassan.nasser@me.com>
2021-03-01 22:20:50 +0100

    updated all

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
-    req = requests.get(url, auth=(argv[1], argv[2])).json()
+    req = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
     print(req.get('id'))
