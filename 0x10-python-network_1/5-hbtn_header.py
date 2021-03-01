0ea93dbdad7b8190853b526f3ceae4f1d3f8506c
nasserdr <hassan.nasser@me.com>
2021-03-01 20:35:49 +0100

    5 updated

--- 0x10-python-network_1/5-hbtn_header.py
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script that fetches X-Request-Id from
+https://intranet.hbtn.io/status
 """
 
 import requests
 import sys
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
