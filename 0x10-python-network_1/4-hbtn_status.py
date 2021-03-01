13882a3f1f178e9fad45d417a95011bf30463a34
nasserdr <hassan.nasser@me.com>
2021-03-01 20:29:54 +0100

    4 updated

--- 0x10-python-network_1/4-hbtn_status.py
+++ 0x10-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("    - type: {}".format(type(req.text)))
-    print("    - cotent: {}".format(req.text))
+    print("    - content: {}".format(req.text))
---
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+
+import requests
+import sys
