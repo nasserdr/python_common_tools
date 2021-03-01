0229c499fec82bbf66c49e1da8bfca8688905d0e
nasserdr <hassan.nasser@me.com>
2021-03-01 19:43:33 +0100

    3 updated

---
+++ 0x10-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8).
+In addition, it handles HTTPError exceptions to print
+the HTTP Status Code, if an error occurs.
+"""
+
+from sys import argv
+from urllib import request, error
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as req:
+            print(req.read().decode("utf-8"))
+    except error.HTTPError:
+        headers = req.info()
+        print("Error code: {}".format(headers.get('status_code')))
