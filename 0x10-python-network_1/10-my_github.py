b35b4e91da7e9c1956faf6496cda723e118dd5b2
nasserdr <hassan.nasser@me.com>
2021-03-01 22:04:48 +0100

    10 updated

--- 0x10-python-network_1/10-my_github.py
+++ 0x10-python-network_1/10-my_github.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script access github
 """
 
-import urllib.request
+import requests
+import sys
 
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()
-   print("Body response:")
-   print("\t- type: {}".format(req.encoding))
-
-
- #Body response:$
-#    - type: <class 'bytes'>$
-#    - content: b'OK'$
-#    - utf8 content: OK$
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2])).json()
+    print(req.get('id'))
