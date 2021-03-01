3e14aa4d0fcbd5cb00a6a12088cf17cf67e0129a
nasserdr <hassan.nasser@me.com>
2021-03-01 19:03:11 +0100

    updated 0 and 1

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
+"""Fetches the URL: https://intranet.hbtn.io/status
 """
 
-import urllib.request
+from urllib.request import Request, urlopen
+
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("    - type: {}".format(type(html)))
-        print("    - Content: {}".format(html))
-        print("    - utf8 content: {}".format(html.decode('utf-8')))
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
