6950528413bee833c64c91241305f3e683c3a2f8
nasserdr <hassan.nasser@me.com>
2021-03-02 08:40:04 +0100

    udated

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(html)))
-        print("    - Content: {}".format(html))
-        print("    - utf8 content: {}".format(html.decode('utf-8')))
+        print("- type: {}".format(type(html)))
+        print("- Content: {}".format(html))
+        print("- utf8 content: {}".format(html.decode('utf-8')))
