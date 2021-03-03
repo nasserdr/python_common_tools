d91fbba881314a849ec5d47c33cb78812b3f4091
nasserdr <hassan.nasser@me.com>
2021-03-03 22:17:19 +0100

    updateds

--- 0x12-SQL_introduction/7-insert_value.sql
+++ 0x12-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- inserts a row
 USE hbtn_0c_0;
+-- inserts
 INSERT INTO first_table (id, name) VALUES (98, "Holberton School");
--- 0x12-SQL_introduction/9-full_creation.sql
+++ 0x12-SQL_introduction/9-full_creation.sql
@@ -4,6 +4,7 @@ CREATE TABLE second_table(
   name VARCHAR(256),
   score INT
 );
+--inserts
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
