437a6f2d4a4a79b2b3fa4a4c31fed9dddf4e124c
nasserdr <hassan.nasser@me.com>
2021-03-03 21:22:38 +0100

    updated 0

--- 0x12-SQL_introduction/0-list_databases.sql
+++ 0x12-SQL_introduction/0-list_databases.sql
@@ -1,6 +1,3 @@
-/*
-* lists all databases of your MySQL server
-*/
-
+/* lists all databases of your MySQL server*/
 SHOW databases;
 /*Show all databases*/
---
+++ 0x12-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+/*
+* lists all databases of your MySQL server
+*/
+
+SHOW databases;
+/*Show all databases*/
