7ba71c671edb59273a475ebb6b04493386365c05
nasserdr <hassan.nasser@me.com>
2021-03-03 22:24:21 +0100

    10/11 updated

--- 0x12-SQL_introduction/11-best_score.sql
+++ 0x12-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- display the content of second_table
-SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
