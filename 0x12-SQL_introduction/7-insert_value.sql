a2ecf4bb52d23582812bd5bd7b8d2f398a17c58b
nasserdr <hassan.nasser@me.com>
2021-03-03 23:02:16 +0100

    7 updated

--- 0x12-SQL_introduction/7-insert_value.sql
+++ 0x12-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- inserts a new record
-USE hbtn_0c_0;
-INSERT INTO first_table (id, name) VALUES (98, "Holberton School");
+INSERT INTO hbtn_0c_0.first_table (id, name) VALUES (98, "Holberton School");
