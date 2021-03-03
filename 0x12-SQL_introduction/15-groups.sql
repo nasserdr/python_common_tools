c5c8900bdfd19b0e8c521ac74ad2f245f8e9c9a2
nasserdr <hassan.nasser@me.com>
2021-03-03 22:43:45 +0100

    udpated

--- 0x12-SQL_introduction/15-groups.sql
+++ 0x12-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- group by score!
-SELECT score, COUNT(*) FROM second_table AS number GROUP BY score ORDER BY DESC;
+SELECT score, COUNT(*)
+FROM second_table AS score, number
+GROUP BY score
+ORDER BY score DESC;
