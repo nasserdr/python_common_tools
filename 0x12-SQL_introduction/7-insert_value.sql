edeab23a4cfc2533df5ec7f6534a7832f3728a27
nasserdr <hassan.nasser@me.com>
2021-03-03 23:00:30 +0100

    udated

--- 0x12-SQL_introduction/7-insert_value.sql
+++ 0x12-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- inserts a row
-INSERT INTO first_table
-VALUES (98, "Holberton School");
+-- inserts a new record
+USE hbtn_0c_0;
+INSERT INTO first_table (id, name) VALUES (98, "Holberton School");
