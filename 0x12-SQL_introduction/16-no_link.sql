83b9c82b3e8304d2c238004d5d37745a48a9991d
nasserdr <hassan.nasser@me.com>
2021-03-03 22:49:35 +0100

    updated

--- 0x12-SQL_introduction/16-no_link.sql
+++ 0x12-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- display the content of second_table
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY name ASC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY name ASC;
