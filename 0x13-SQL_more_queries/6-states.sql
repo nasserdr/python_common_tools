b82920f4fe47e5f25e92b7d72b366fb5679a5db9
nasserdr <hassan.nasser@me.com>
2021-03-05 20:14:49 +0100

    6 updated

--- 0x13-SQL_more_queries/6-states.sql
+++ 0x13-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL
