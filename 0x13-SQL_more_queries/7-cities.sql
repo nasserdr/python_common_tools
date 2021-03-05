c860deab9db6b1691d35baeedacaccfaa5a91c1e
nasserdr <hassan.nasser@me.com>
2021-03-05 20:16:42 +0100

    7 up

--- 0x13-SQL_more_queries/7-cities.sql
+++ 0x13-SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
 -- creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   state_id INT NOT NULL FOREIGN KEY REFERENCES table(id)
-  name VARCHAR(256) NOT NULL);
-  
+  name VARCHAR(256) NOT NULL
+);
