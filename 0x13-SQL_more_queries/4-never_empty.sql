d9fcdcb287f53726dde77f59681721ce50c47497
nasserdr <hassan.nasser@me.com>
2021-03-05 20:11:54 +0100

    6 updated

--- 0x13-SQL_more_queries/4-never_empty.sql
+++ 0x13-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,5 @@
 -- Insure that the field is never empty
-CREATE TABLE IF NOT EXISTS id_not_null ( id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS id_not_null (
+  id INT DEFAULT 1,
+  name VARCHAR(256) NOT NULL
+);
