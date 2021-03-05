143c52616f444a5801fa924c0d724c958a3ef902
nasserdr <hassan.nasser@me.com>
2021-03-05 20:00:25 +0100

    2 updated

--- 0x13-SQL_more_queries/2-create_read_user.sql
+++ 0x13-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creates a user with read access
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO USER 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost'
