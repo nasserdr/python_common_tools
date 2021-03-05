baca19c1cd2451184345e6219c946ea06cdefb43
nasserdr <hassan.nasser@me.com>
2021-03-05 20:23:53 +0100

    8 up

--- 0x13-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x13-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lsts all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT FROM hbtn_0d_usa
 WHERE name = California
-ORDER BY cities.id ASCE
+ORDER BY `cities`.`id` ASCE
