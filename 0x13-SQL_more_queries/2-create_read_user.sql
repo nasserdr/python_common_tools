5495061000755d5e391dfde3ef8495e2687c795c
nasserdr <hassan.nasser@me.com>
2021-03-05 20:08:34 +0100

    2 updated

--- 0x13-SQL_more_queries/2-create_read_user.sql
+++ 0x13-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
--- Creates a user with read access
+-- Creates the database `hbtn_0d_2` and
+-- the user `user_0d_2` with specific privileges.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
