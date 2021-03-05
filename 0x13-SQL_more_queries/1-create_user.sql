036bbca9c888d410c0c4aaffa85fef3febe12498
nasserdr <hassan.nasser@me.com>
2021-03-05 19:46:49 +0100

    updated 1

--- 0x13-SQL_more_queries/1-create_user.sql
+++ 0x13-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
