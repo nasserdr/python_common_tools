36b9780281aa1d0b3879d8418dd86ff6960a397b
nasserdr <hassan.nasser@me.com>
2021-03-05 20:13:00 +0100

    5 updated

--- 0x13-SQL_more_queries/5-unique_id.sql
+++ 0x13-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
 -- Insure that the field is never empty and the ID is unique
-CREATE TABLE IF NOT EXISTS unique_id ( id INT UNIQUE DEFAULT 1, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT UNIQUE DEFAULT 1,
+  name VARCHAR(256) NOT NULL
+);
