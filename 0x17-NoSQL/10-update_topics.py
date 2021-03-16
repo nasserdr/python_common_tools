eb2e8fae9b507abf1118e7ef3ba61b1e764ac559
nasserdr <hassan.nasser@me.com>
2021-03-16 07:20:57 +0100

    10 updated for pep

--- 0x17-NoSQL/10-update_topics.py
+++ 0x17-NoSQL/10-update_topics.py
@@ -3,10 +3,11 @@
 changes all topics of a school document based on the name
 """
 
+
 def update_topics(mongo_collection, name, topics):
     """
     changes all topics
     """
-    query = {"name" : name}
-    newvalues = { "$set": { "topics":topics } }
+    query = {"name": name}
+    newvalues = {"$set": {"topics": topics}}
     mongo_collection.update_one(query, newvalues)
