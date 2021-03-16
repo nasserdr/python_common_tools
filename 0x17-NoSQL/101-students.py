9381d80dc1bd46304a23f94c79bfdff3f6c365d5
nasserdr <hassan.nasser@me.com>
2021-03-16 22:37:48 +0100

    101 updated

---
+++ 0x17-NoSQL/101-students.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+    """
+    returns all students sorted by average score
+    """
+    return mongo_collection.find({}).sort("averageScore")
