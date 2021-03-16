121982be7f625eea2919ebfda34c3816e9d00bc9
nasserdr <hassan.nasser@me.com>
2021-03-16 22:24:21 +0100

    11 updated

--- 0x17-NoSQL/11-schools_by_topic.py
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -8,5 +8,5 @@ def schools_by_topic(mongo_collection, topic):
     """
     returns the list of school having a specific topic
     """
-    q = {'topics': topic}
+    q = {'topics': {'$elemMatch': {'$eq': topic}}}
     return mongo_collection.find(q)
