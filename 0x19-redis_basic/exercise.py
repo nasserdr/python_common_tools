18b6dcb0b4e1eb5aa160f3795d4f85bf2fb6c4c3
nasserdr <hassan.nasser@me.com>
2021-03-22 22:27:58 +0100

    1 updated

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Create a Cache class
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -18,10 +18,13 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, float, int, str]):
+    def store(self, data: Union[str, bytes, int, float]):
         """
         takes data and store it with a key
         """
         key = uuid.uuid1()
         self._redis.set(str(key), str(data))
         return str(key)
+
+    def get(self, key: str, fn: Optional[Callable]):
+        self._redis.get(key)
